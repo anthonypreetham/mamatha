7d6d01072cd7130cf0b3de62752d39e39c31d1fa
anthony <anthonypreethma@gmail.com>
2023-09-13 23:31:59 +0530

    comment

---
+++ src/main/java/com/docker/project/dcrproject/DcrprojectApplication.java
@@ -0,0 +1,13 @@
+package com.docker.project.dcrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcrprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcrprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/project/dcrproject/DcrprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.project.dcrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcrprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
